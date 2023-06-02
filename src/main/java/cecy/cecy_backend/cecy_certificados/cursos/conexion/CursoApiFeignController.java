5d3a9e9f6ceb469ff3a85d5490335ffc77b55b79
Soy-Anderson <ajr.parra@yavirac.edu.ec>
2023-06-02 18:46:43 -0500

    add for endpoint

--- src/main/java/cecy/cecy_backend/cecy_certificados/cursos/conexion/CursoApiFeignController.java
+++ src/main/java/cecy/cecy_backend/cecy_certificados/cursos/conexion/CursoApiFeignController.java
@@ -1,5 +1,6 @@
 package cecy.cecy_backend.cecy_certificados.cursos.conexion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +21,10 @@ public class CursoApiFeignController {
     @GetMapping("/courses/state-course/{state}")
     public  List<Course> getCoursesByState(@PathVariable String state){
         return cursoApiFeignService.getCoursesByState(state);
+    }
+
+    @GetMapping("/courses/{id}")
+    public List<Course> getCourseId(@PathVariable("id") Integer id) {
+        return  cursoApiFeignService.getCourseById(id);
     }
 }
