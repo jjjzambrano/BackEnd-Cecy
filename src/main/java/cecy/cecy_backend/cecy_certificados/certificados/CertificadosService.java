fb6bd78a40e8fbc200255ca715f660f40a6dffd3
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2023-07-17 15:59:29 -0500

    Actualizacion

--- src/main/java/cecy/cecy_backend/cecy_certificados/certificados/CertificadosService.java
+++ src/main/java/cecy/cecy_backend/cecy_certificados/certificados/CertificadosService.java
@@ -66,6 +66,82 @@ public class CertificadosService {
         String patternFinish = "dd, MMMM, yyyy";
         Locale locale = new Locale("es", "EC");
         SimpleDateFormat fromUser = new SimpleDateFormat("yyyy-MM-dd");
+        if (codigos.getCertificado().getTipoCertificado().getTipo().equals("Cecy")){
+            if (codigos.getCertificado().getTipoCertificado().getFirmas().size() == 2) {
+                Estudiantes persona = customerPerson.findById(codigos.getMatriculas().getEstudiantes().getId());
+                reportParameters.put("nombres_completos", persona.getNombres() + " " + persona.getApellidos());
+                Curso curso = customerCourse.findById(codigos.getMatriculas().getCursoId());
+                Planificacion planificacion = planificationService.getPlanificationId(curso.getPlanificationId());
+                reportParameters.put("curso_nombre", planificacion.getName());
+                try {
+                    SimpleDateFormat simpleDateFormartStart = new SimpleDateFormat(patternStart,locale);
+                   String fechaInicio = simpleDateFormartStart.format(fromUser.parse(planificacion.getStartDate()));
+                    SimpleDateFormat simpleDateFormartFinish = new SimpleDateFormat(patternFinish, locale);
+                String fechaFin = simpleDateFormartFinish.format(fromUser.parse(planificacion.getFinishDate()));
+                reportParameters.put("fecha_curso",fechaInicio + " al " + fechaFin);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                reportParameters.put("hora", Integer.toString(planificacion.getDurationTime()));
+                int participanteIndex = 0;
+                for (FirmaTipoCertificado participante : codigos.getCertificado().getTipoCertificado().getFirmas()) {
+                    if(participanteIndex == 1){
+                        reportParameters.put("participante2", participante.getFirma().getNombres() + " " + participante.getFirma().getApellidos());
+                        reportParameters.put("tipo_participante2", participante.getRol());
+                        reportParameters.put("firma2","classpath:images/"+ participante.getFirma().getFirma());
+                        System.out.print("classpath:images/"+ participante.getFirma().getFirma());
+                    }
+                    if(participanteIndex == 0){
+                        participanteIndex = 1;
+                        reportParameters.put("participante1", participante.getFirma().getNombres() + " " + participante.getFirma().getApellidos());
+                    reportParameters.put("tipo_participante1", participante.getRol());
+                    reportParameters.put("firma1","classpath:images/"+ participante.getFirma().getFirma());
+                    try {
+                        SimpleDateFormat simpleDateFormartStart = new SimpleDateFormat(patternStart,locale);
+                       String fechaInicio = simpleDateFormartStart.format(fromUser.parse(planificacion.getStartDate()));
+                        SimpleDateFormat simpleDateFormartFinish = new SimpleDateFormat(patternFinish, locale);
+                    String fechaFin = simpleDateFormartFinish.format(fromUser.parse(planificacion.getFinishDate()));
+                    reportParameters.put("Qr","Nombre Estudiante: "+ persona.getNombres() + " " + persona.getApellidos()+" Nombre del Curso: "+ planificacion.getName() +" Registro Cecy No: "+ codigos.getCodigo()+ " Nombre del Representante Legal: "+participante.getFirma().getNombres()+" Fecha del Curso: "+" del " + fechaInicio + " al "+ fechaFin);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                  ;
+                    System.out.print("classpath:images/"+ participante.getFirma().getFirma());
+                    }
+                    
+                }
+                String dia = "dd";
+                String mes = "MMMM";
+                String año = "yyyy";
+                Date dateCertificate = java.util.Date.from(codigos.getCertificado().getFecha().atStartOfDay()
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+                SimpleDateFormat Dateday = new SimpleDateFormat(dia,locale);
+                String onlyDay = Dateday.format(dateCertificate);
+                SimpleDateFormat DateMonth = new SimpleDateFormat(mes,locale);
+                String onlyMonth = DateMonth.format(dateCertificate);
+               SimpleDateFormat DateYear = new SimpleDateFormat(año,locale);
+               String onlyYear = DateYear.format(dateCertificate);
+
+                reportParameters.put("fecha_certificado", onlyDay + " de "+ onlyMonth+ " de "+ onlyYear);
+                reportParameters.put("codigo", codigos.getCodigo());
+                
+                
+               
+                try {
+                    reportJasperPrint = JasperFillManager.fillReport(
+                            JasperCompileManager.compileReport(
+                                    ResourceUtils.getFile("classpath:jrxml/cecy_2participantes.jrxml")
+                                            .getAbsolutePath()) // path of the jasper report
+                            , reportParameters // dynamic parameters
+                            , new JREmptyDataSource());
+                } catch (FileNotFoundException | JRException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
         if (codigos.getCertificado().getTipoCertificado().getTipo().equals("Senecyt")) {
 
             if (codigos.getCertificado().getTipoCertificado().getFirmas().size() == 2) {
